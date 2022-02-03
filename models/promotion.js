14faf9b2c666d126cd29f13def56ee559c04e64f
Nepsydez <sudeepbishwakarma@hotmail.com>
2022-02-03 16:20:49 -0500

    Server

---
+++ models/promotion.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose"); 
+const Schema = mongoose.Schema; 
+
+require("mongoose-currency").loadType(mongoose);
+const Currency = mongoose.Types.Currency
+
+const promotionSchema = new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      image: {
+        type: Image,
+        required: true,
+      },
+      featured: {
+        type: Boolean,
+
+      },
+      cost: {
+        type: Currency,
+        required: true,
+
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  const Promotion = mongoose.model("Promotion", promotionSchema);
+  
+  module.exports = Promotion;
